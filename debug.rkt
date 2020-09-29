4f0698f2bcb614f9d6a981741b4b0763e82dca75
Gabriel Poesia Reis e Silva <me@gpoesia.com>
2020-09-29 12:35:40 -0700

    debug: allow filtering messages by regex

--- debug.rkt
+++ debug.rkt
@@ -7,15 +7,19 @@
 
 ; Change to true to enable debugging
 (define debug? #f)
+; Only print messages that contain this pattern.
+(define debug-pattern #rx"")
 
 (define-syntax-rule (log-debug message args ...)
   (if debug?
     (let ([loc (quote-srcloc)])
-      (printf (string-append "[~a:~a@~a]: " message)
-              (source-location-source loc)
-              (source-location-line loc)
-              (current-date-string-iso-8601 #t)
-              args ...))
+      (if (regexp-match debug-pattern message)
+        (printf (string-append "[~a:~a@~a]: " message)
+                (source-location-source loc)
+                (source-location-line loc)
+                (current-date-string-iso-8601 #t)
+                args ...)
+        (void)))
     (void)))
 
 (provide log-debug)
